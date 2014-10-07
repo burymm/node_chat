224efb6abad52d4fe07e9daf083aab83e4f01380
mikalai bury <burymm@gmail.com>
2014-10-07 12:50:12 +0300

    first working version

--- js/main.js
+++ js/main.js
@@ -2,32 +2,75 @@
  * Created by mbury on 10/6/2014.
  */
 
-function addMessage(message) {
+var connection = new WebSocket('ws://127.0.01:8124');
+
+
+function addMessage(data) {
     var $chat = $('#chat'),
         $li = $('<li>');
-    $li.text(message);
+    $li.text(data.user + ' ' + data.message);
+    if (data.user == user) {
+        $li.addClass('own');
+    } else {
+        $li.addClass('enemy');
+    }
     $chat.prepend($li);
 }
 
+function sendMessage() {
+    var $message = $('#message');
+    connection.send(JSON.stringify({user: user, message: $message.val()}));
+    $message.val('');
+}
+
+
+var user = 'user.' + Math.floor(Math.random() * 1000);
 
 
 $(function () {
     $('#chat').html('');
+    $('#user-name').html(user);
 
-    var connection = new WebSocket('ws://127.0.01:8124');
+
+
 
     connection.onopen = function () {
-        connection.send('Ping'); // Send the message 'Ping' to the server
+        connection.send(JSON.stringify({'user': user, option: 'connect'})); // Send the message 'Ping' to the server
     };
 
     connection.onmessage = function (data, flags) {
-        var parsedData = JSON.parse(data.data)
-        console.log('received', parsedData.message, flags);
-        addMessage(parsedData.message);
+
+
+        try {
+            var parsedData = JSON.parse(data.data);
+            if (typeof parsedData == 'string') {
+                parsedData = JSON.parse(parsedData);
+            }
+/*            console.log('received', parsedData);*/
+            if (parsedData.option) {
+                switch (parsedData.option) {
+                    case 'connect':
+                        parsedData.message = ' connected';
+                    break;
+                    case 'disconnect':
+                        parsedData.message = ' disconnected.';
+                    break;
+                }
+            }
+            addMessage(parsedData);
+        } catch (exception) {
+
+        }
+
+
     };
 
 
-    $('#send-message').on('click', function() {
-        connection.send($('#message').val());
+    $('#send-message').on('click', sendMessage);
+
+    $('#message').on('keypress', function(event) {
+        if (event.which == 13) {
+            sendMessage();
+        }
     });
 });
--- websocketserver.js
+++ websocketserver.js
@@ -7,24 +7,37 @@ var clients = [];
 var WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({port: 8124});
 
-function sendAll(message) {
+process.stdout.write("\u001b[2J\u001b[0;0H"); // clear console
+
+function sendAll(data) {
     for (var i = 0; i < clients.length; i += 1) {
-        clients[i].send(JSON.stringify({message: message}));
-        console.log('send to ',clients[i]._sender._socket. _monotonicStartTime);
+        try {
+            clients[i].send(JSON.stringify(data));
+        } catch (expection) {
+            if (expection == 'Error: not opened') {
+                var user = clients[i].user;
+                clients.splice(i, 1);
+                sendAll(JSON.stringify({user: user, option: 'disconnect'}));
+            }
+        }
     }
 }
 
 
 wss.on('connection', function(ws) {
     console.log('server started');
-
     clients.push(ws);
-    console.log(ws._sender._socket. _monotonicStartTime);
-
-    ws.on('message', function(message) {
-        console.log('received: %s', message);
-        sendAll(message);
+    ws.on('message', function(data) {
+        try {
+            console.log('received: %s', data);
+            var data = JSON.parse(data);
+            sendAll(data);
+            if (data.option == 'connect') {
+                this.user = data.user;
+                console.log(this.user, data.message);
+            }
+        } catch (exception) {
+            console.log(exception);
+        }
     });
-
-    ws.send('something');
 });
